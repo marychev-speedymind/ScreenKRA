d3507d5f024becfeebc7f58d32fa2a86181828e2
MikhailMarychev <mikhail.marychev@speedymind.net>
2021-04-14 18:54:45 +0300

    Craete Screen class and move a logic to it

--- ScreenKRA/Program.cs
+++ ScreenKRA/Program.cs
@@ -1,171 +1,47 @@
 ﻿using System;
 using System.IO;
-using System.IO.Compression;
-using System.Linq;
 using ImageMagick;
-using System.Security.Cryptography;
 
 
 namespace ScreenKRA
 {
-
-    class Screen
-    {
-
-        public string WorkDir;
-        public string OutputDir;
-        public string TargetImage = "mergedimage.png";
-
-        public void InitWorkDir()
-        {
-            Console.WriteLine("Set directory to look for `*kra` files:");
-            // string path = Path.GetFullPath(Console.ReadLine());
-            string path = Path.GetFullPath(@"C:/mmc");
-            if (!Directory.Exists(path))
-            {
-                string errorText = $"[ERROR]: {path} Dir was not found!";
-                Console.WriteLine(errorText);
-                throw new DirectoryNotFoundException(errorText);
-            }
-
-            this.WorkDir = path;
-        }
-    }
-
     class Program
     {
-        /*
-        static void Crop2(MagickImage magickImage, string path, int chunk, int spacing)
-        {
-            int width = magickImage.Width;
-            MagickGeometry geometry = new MagickGeometry();
-
-            geometry.Width = chunk;
-            geometry.Height = magickImage.Height;
-            geometry.Y = 0;
-
-            byte index = 0;
-            while (width > 0)
-            {
-                geometry.X += (chunk + spacing);
-                index += 1;
-
-                CreateScreen2(magickImage, geometry, path, index);
-                width -= (chunk + spacing);
-            }
-        }
-        static void CreateScreen2(MagickImage image, MagickGeometry geometry, string path2, byte index)
-        {
-            var screen = image.Clone(geometry);
-            screen.Strip();
-
-            screen.Write(new FileInfo(Path.Combine(@"C:\Temp", Guid.NewGuid().ToString() + ".png")), MagickFormat.Png);
-            //Console.WriteLine("\t[+] Created a new screen: {0}", fullPathScreen);
-        }
-        */
-
-        static void Crop(MagickImage magickImage, string path, int chunk, int spacing)
-        {
-            // create parent dir to list og images
-            string dir = $"{chunk}x{magickImage.Height}";
-            string newPath = Path.GetFullPath(path + "/" + dir);
-            if (!Directory.Exists(newPath))
-                Directory.CreateDirectory(newPath);
-            Console.WriteLine($"[+] newPath directory: {newPath}");
-
-            int width = magickImage.Width;
-            MagickGeometry geometry = new MagickGeometry();
-
-            geometry.Width = chunk;
-            geometry.Height = magickImage.Height;
-            geometry.Y = 0;
-
-            byte index = 0;
-            while (width > 0)
-            {
-                geometry.X += (chunk + spacing);
-                index += 1;
-
-                CreateScreen(magickImage, geometry, newPath, index);
-                width -= (chunk + spacing);
-            }
-        }
-
-        static FileInfo GetFullPathScreen(string path, MagickGeometry g, byte index)
-        {
-            string fileName = $"{index}.png";
-            return new FileInfo($"{path}\\{fileName}");
-        }
-
-        static void CreateScreen(MagickImage image, MagickGeometry geometry, string path, byte index)
-        {
-            var screen = image.Clone(geometry);
-            screen.Strip();
-
-            FileInfo fullPathScreen = GetFullPathScreen(path, geometry, index);
-            screen.Write(fullPathScreen, MagickFormat.Png);
-            Console.WriteLine("\t[+] Created a new screen: {0}", fullPathScreen);
-        }
-
         static void Main(string[] args)
         {
+            Screen screen = new Screen();
+            
+            screen.InitWorkDir();
+            screen.InitOutputDir();
 
-            string TargetImage = "mergedimage.png";
-
-            Console.WriteLine("Set directory to look for `*kra` files:");
-            // string workDir = Path.GetFullPath(Console.ReadLine());
-            string workDir = Path.GetFullPath(@"C:/mmc");
-            if (!Directory.Exists(workDir))
+            foreach (string fullPath in Directory.EnumerateFiles(screen.WorkDir, "*.kra", SearchOption.AllDirectories))
             {
-                string errorText = $"[ERROR]: {workDir} Dir was not found!";
-                Console.WriteLine(errorText);
-                throw new DirectoryNotFoundException(errorText);
-            }
-            Console.WriteLine($"[+] Work directory: {workDir}");
-
-            Console.WriteLine("Set directory to results:");
-            // string outDir = Path.GetFullPath(Console.ReadLine());
-            string outDir = Path.GetFullPath(@"C:/KRA");
-            if (!Directory.Exists(outDir))
-                Directory.CreateDirectory(outDir);
-            Console.WriteLine($"[+] Output directory: {outDir}");
-
-            foreach (string fullPath in Directory.EnumerateFiles(workDir, "*.kra", SearchOption.AllDirectories))
-            {
-                // prepare and create only folders
-                /// [note] For OS: window: replace roor dirs
-                string workDirWithoutDisk = workDir.Replace(Path.GetPathRoot(workDir), "");
-                string fullWorkPath = Path.GetDirectoryName($"{Path.GetFullPath(fullPath).Replace(workDir, (outDir + '/' + workDirWithoutDisk))}");
-
-                if (!Directory.Exists(fullWorkPath))
-                {
-                    Directory.CreateDirectory(fullWorkPath);
-                    Console.WriteLine("[+] Created a new dir: {0}", fullWorkPath);
-                }
-
-                // get the screen needed
-                using var file = File.OpenRead(fullPath);
-                using var zip = new ZipArchive(file, ZipArchiveMode.Read);
-                var entry = zip.Entries.Where(x => x.ToString() == TargetImage).FirstOrDefault();
-                using MagickImage image = new MagickImage(entry.Open());
-
-                // rules to crop
+                screen.InitAndCreateFullOutputPath(fullPath);
+                
+                using MagickImage image = screen.GetTagretImage(fullPath);
                 switch (image.Height)
                 {
                     case 2208:
                     case 2688:
-                        Crop(image, fullWorkPath, 1242, 52);
+                        screen.Crop(image, Path.GetFileNameWithoutExtension(fullPath), 1242, 52);
                         break;
                     case 2732:
-                        Crop(image, fullWorkPath, 2732, 255);
+                        screen.Crop(image, Path.GetFileNameWithoutExtension(fullPath), 2732, 255);
                         break;
                     default:
-                        Console.WriteLine($"[INFO] The {image.FileName} image doesn't contain the heights needed");
+                        screen.CreateNativeScreen(image, Path.GetFileNameWithoutExtension(fullPath));
                         break;
                 }
             }
 
+            END();
+        }
+
+        static void END()
+        {
+            Console.WriteLine("----------------------------------");
             Console.WriteLine("Creating screenshots was finished!");
+            Console.WriteLine("----------------------------------");
         }
     }
 }
---
+++ ScreenKRA/Screen.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+using ImageMagick;
+
+
+namespace ScreenKRA
+{
+    public class Screen
+    {
+        public string WorkDir;
+        public string OutputDir;
+        public string FullOutputPathToDir;
+        public static readonly string TargetImage = "mergedimage.png";
+
+        public void InitAndCreateFullOutputPath(string fullPath)
+        {
+            /// [note] For OS: window: replace roor dirs
+            string workDirWithoutDisk = WorkDir.Replace(Path.GetPathRoot(WorkDir), "");
+            string pathToDir = Path.GetDirectoryName($"{Path.GetFullPath(fullPath).Replace(WorkDir, (OutputDir + '/' + workDirWithoutDisk))}");
+            
+            this.FullOutputPathToDir = pathToDir;
+            CreateDir(this.FullOutputPathToDir);
+        }
+
+        public MagickImage GetTagretImage(string fullPath)
+        {
+            using var file = File.OpenRead(fullPath);
+            using var zip = new ZipArchive(file, ZipArchiveMode.Read);
+            var entry = zip.Entries.Where(x => x.ToString() == TargetImage).FirstOrDefault();
+            return new MagickImage(entry.Open());
+        }
+
+        public static void CreateDir(string pathToDir)
+        {
+            if (!Directory.Exists(pathToDir))
+            {
+                Directory.CreateDirectory(pathToDir);
+                Console.WriteLine($"[+] Created the directory: {pathToDir}");
+            }
+        }
+
+        // public 
+
+        public void CreateNativeScreen(MagickImage image, string fileName)
+        {
+            MagickGeometry geometry = new MagickGeometry(width: image.Width, height: image.Height);
+            CreateScreen(image, geometry, FullOutputPathToDir, fileName);
+            Console.WriteLine($"[INFO] The {image.FileName} image doesn't contain the heights needed");
+        }
+
+        public void Crop(MagickImage magickImage, string fileName, int chunk, int spacing)
+        {
+            // create parent dir to list of images
+            string dir = $"{fileName}({chunk}x{magickImage.Height})";
+            string fullPathToDir = Path.GetFullPath(FullOutputPathToDir + "/" + dir);
+            CreateDir(fullPathToDir);
+
+            MagickGeometry geom = new MagickGeometry(width: chunk, height: magickImage.Height);
+
+            short itemWidth = (short)(chunk + spacing);
+            int width = magickImage.Width;
+            
+            byte num = 0;
+            while (width > 0)
+            {
+                CreateScreen(magickImage, geom, fullPathToDir, num.ToString());
+
+                geom.X += itemWidth;
+                num += 1;
+                width -= itemWidth;
+            }
+
+            if (Math.Abs(width) == Math.Abs(spacing))
+            {
+                Console.WriteLine($"\r\nRemainder: {width} / Image width: {magickImage.Width}\r\n");
+                Console.WriteLine("Crop finished successfuly!");
+            } else
+            {
+                Console.WriteLine($"[WARNING] Crop: Troubles with an image's width: {magickImage.Width}!");
+            }
+
+        }
+
+        public void InitWorkDir()
+        {
+            Console.WriteLine("Set directory to look for `*kra` files:");
+            string path = Path.GetFullPath(Console.ReadLine());
+            if (!Directory.Exists(path))
+            {
+                string errorText = $"[ERROR] Dir was not found! {path} ";
+                Console.WriteLine(errorText);
+                throw new DirectoryNotFoundException(errorText);
+            }
+
+            this.WorkDir = path;
+            Console.WriteLine($"[+] Work directory: {path}");
+        }
+
+        public void InitOutputDir()
+        {
+            Console.WriteLine("Set directory to results:");
+            string outDir = Path.GetFullPath(Console.ReadLine());
+            CreateDir(outDir);
+            this.OutputDir = outDir;
+        }
+
+        // private
+
+        private static FileInfo GetFullPathScreen(string pathToDir, string fileName) => new FileInfo($"{pathToDir}\\{fileName}.png");
+
+        private static void CreateScreen(MagickImage image, MagickGeometry geometry, string pathToDir, string fileName)
+        {
+            var screen = image.Clone(geometry);
+            screen.Strip();
+
+            FileInfo fullPathScreen = GetFullPathScreen(pathToDir, fileName);
+            screen.Write(fullPathScreen, MagickFormat.Png);
+            Console.WriteLine("\t[+] Created a new screen: {0}", fullPathScreen);
+        }
+    }
+
+
+}
